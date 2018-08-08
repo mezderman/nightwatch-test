fa464f3ed6c4e0f308759a697fe4dc525efb1ce2
Moshe Ezderman <moshe.ezderman@whiteops.com>
2018-08-08 17:23:47 -0400

    adding more commands

--- tests/fixtures/simpleTest.js
+++ tests/fixtures/simpleTest.js
@@ -1,16 +1,20 @@
 module.exports = {
   'Github Repositories Navigation': function (browser) {
     browser
-      .url('https://github.com/mezderman')
+      .url('https://github.com/mezderman', function () {
+        console.log('go to url')
+      })
+      .getValue('body', function (result) {
+        console.log('got body')
+      })
       .saveScreenshot('reports/screenshots/github.png', function (data) {
         console.log('image saved')
         browser.url('http://www.google.com')
       })
-
-    browser.perform((browser, done) => {
-      console.log('end')
-      browser.end()
-      done()
-    })
+      .perform((browser, done) => {
+        console.log('end')
+        browser.end()
+        done()
+      })
   }
 }
